4cdc302d67f855835fefd3aeacbd53f09ae67f74
oparisy <olivier.parisy@gmail.com>
2016-12-29 15:40:26 +0100

    Extra features and documentation

--- index.js
+++ index.js
@@ -12,12 +12,19 @@ var two = new Two({ width: 285, height: 200 }).appendTo(elem)
 var circle = two.makeCircle(-70, 0, 50)
 var rect = two.makeRectangle(70, 0, 100, 100)
 circle.fill = '#FF8000'
+circle.stroke = 'orangered'
 rect.fill = 'rgba(0, 200, 255, 0.75)'
+rect.stroke = '#1C75BC'
 
+// Groups can take an array of shapes and/or groups.
 var group = two.makeGroup(circle, rect)
+
+// And have translation, rotation, scale like all shapes.
 group.translation.set(two.width / 2, two.height / 2)
 group.scale = 0
-group.noStroke()
+
+// You can also set the same properties a shape have.
+group.linewidth = 5
 
 // Bind a function to scale and rotate the group
 // to the animation loop.
@@ -30,4 +37,8 @@ two.bind('update', function (frameCount) {
   var t = (1 - group.scale) * 0.125
   group.scale += t
   group.rotation += t * 4 * Math.PI
-}).play()  // Finally, start the animation loop
+})
+
+// Finally, start the animation loop:
+// call update() 60 times per second
+two.play()
